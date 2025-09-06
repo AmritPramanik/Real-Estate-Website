de61289a27efba682fbbc354ca1e842dd3d1703b
Amrit Pramanik <pramanikamrit778@gmail.com>
2025-09-07 01:34:09 +0530

    Initial Commit

---
+++ real-estate/src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='container mx-auto py-4 pt-20 px-6 md:px-20 1g:px-32 my-20 w-full overflow-hidden' id='Projects'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span>Completed</span></h1>
+      <p className='text-center text-gray-600'>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
+    </div>
+  )
+}
+
+export default Projects
